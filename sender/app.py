4d2ab660e59d962a4ebc393ac1301b11bf3bc27f
aiden-dai <aiden.dai@gmail.com>
2020-03-18 15:18:26 +0800

    changed to use spark in sender

--- sender/app.py
+++ sender/app.py
@@ -1,54 +1,75 @@
-'''Use Pandas to process the csv file and send the records to a kafka topic.'''
+'''Use Spark to process the csv file and send the records to a kafka topic.'''
 import time
 import json
-import pandas as pd
 
 from confluent_kafka import Producer
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType
 
 
 def delivery_report(err, msg):
     if err is not None:
-        print('Message delivery failed: {}'.format(err))
-    # else:
-    #     print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+        print(f'Message delivery failed: {err}')
+
+
+def send(producer, row):
+
+    producer.poll(0)
+
+    data = {
+        'user_id': row.user_id,
+        'item_id': row.item_id,
+        'behavior_type': row.behavior_type,
+        'item_category': row.item_category,
+        'date': '2019-{}'.format(row.time[5:10]),
+        'hour': int(row.time[-2:])
+    }
+
+
+    # Asynchronously produce a message, the delivery report callback
+    producer.produce('user_log', json.dumps(data).encode(
+        'utf-8'), callback=delivery_report)
+
+    # Add delay of 0.1 seconds per records.
+    # time.sleep(0.1)
 
 
 def main():
-    data = pd.read_csv('data/sample.csv', sep=',')
-    #data = pd.read_csv('data/tianchi_mobile_recommend_train_user.csv', sep=',')
+    spark = SparkSession.builder.appName('Sender').getOrCreate()
 
-    #data = data.sort_values(by='time', ascending=True)
-    print(data.head(5))
+    schema = StructType()\
+        .add("user_id", "integer")\
+        .add("item_id", "integer")\
+        .add("behavior_type", "integer")\
+        .add('user_geohash', 'string')\
+        .add("item_category", "integer")\
+        .add("time", "string")
 
-    print(data.shape)
-    print(data.columns)
+    df = spark.read.csv('data/sample.csv', header="true",
+                        schema=schema).sort('time', ascending=True)
+    # df = spark.read.csv('data/tianchi_mobile_recommend_train_user.csv', header="true", schema=schema)
+
+    # Show schema
+    df.printSchema()
+
+    # Show total number of records
+    count = df.count()
+    print(count)
 
     p = Producer({'bootstrap.servers': '192.168.56.107:9092'})
 
-    for i, row in data.iterrows():
-        data = {
-            'user_id': row.user_id,
-            'item_id': row.item_id,
-            'behavior_type': row.behavior_type,
-            'item_category': row.item_category,
-            'date': '2019-{}'.format(row.time[5:10]),
-            'hour': int(row.time[-2:])
-        }
+    start = time.time()
 
-        p.poll(0)
-
-        # Asynchronously produce a message, the delivery report callback
-        p.produce('user_log', json.dumps(data).encode(
-            'utf-8'), callback=delivery_report)
-
-        # Add delay of 0.1 seconds per records.
-        time.sleep(0.1)
-
-        if (i + 1) % 100 == 0:
-            print('Totally {} records has been sent'.format(i + 1))
+    for row in df.collect():
+        send(p, row)
 
     p.flush()
 
+    end = time.time()
+    total_time = end - start
+
+    print(f'Total time: {str(total_time)}')
 
 if __name__ == "__main__":
     main()
--- streaming/app.py
+++ streaming/app.py
@@ -1,24 +1,23 @@
 '''Use Spark StructedStream to consume kafka topics'''
-from __future__ import print_function
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json
 from pyspark.sql.types import StructType
 
-if __name__ == "__main__":
 
-    bootstrapServers = '192.168.56.107:9092'
+def main():
+
+    servers = '192.168.56.107:9092'
     topics = 'user_log'
 
     spark = SparkSession\
         .builder\
-        .appName("test_kafka")\
+        .appName("streaming")\
         .getOrCreate()
 
     df = spark\
         .readStream\
         .format("kafka")\
-        .option("kafka.bootstrap.servers", bootstrapServers)\
+        .option("kafka.bootstrap.servers", servers)\
         .option('subscribe', topics)\
         .option("startingOffsets", "earliest")\
         .load()\
@@ -41,11 +40,16 @@ if __name__ == "__main__":
 
     count_by_hour = event.groupBy('hour').count().sort('hour')
 
-    # Process Every 5 seconds.
+    # Process Every 30 seconds.
     query = count_by_hour.writeStream\
         .outputMode("complete")\
         .format('console')\
-        .trigger(processingTime='5 seconds')\
+        .trigger(processingTime='30 seconds')\
         .start()
 
     query.awaitTermination()
+
+
+if __name__ == "__main__":
+
+    main()
